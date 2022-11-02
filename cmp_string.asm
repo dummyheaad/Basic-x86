d0c38be155b586b78d185453a39c014886f34ef8
Aysuka Ansari <99653989+dummyheaad@users.noreply.github.com>
2022-11-02 11:32:40 +0700

    Modifikasi cmp_string.asm

--- cmp_string.asm
+++ cmp_string.asm
@@ -4,13 +4,14 @@ global main
 extern strncmp, printf
 
 segment .data
-    str1 db "Hellaaaaaa", 0x00              ; string pertama yang akan dibandingkan
+    str1 db "Helloo", 0x00              ; string pertama yang akan dibandingkan
     len_str1 equ $ - str1                   ; panjang string pertama
-    str2 db "Hello", 0x00                   ; string kedua yang akan dibandingkan
+    str2 db "Helloe", 0x00                   ; string kedua yang akan dibandingkan
     len_str2 equ $ - str2                   ; panjang string kedua
     res db "Comparison 3 first character between %s and %s = %d", 0x0a, 0x00
     len_res equ $ - res
-    tc equ 3                                ; jumlah karakter yang akan dibandingkan
+    tc dd 2                                ; jumlah karakter yang akan dibandingkan
+    len_tc equ $ - tc
 
 segment .text
 main:
@@ -18,12 +19,14 @@ main:
     mov ebp, esp                            
     sub esp, len_str1                       
     sub esp, len_str2                       
-    sub esp, len_res                        
+    sub esp, len_res            
+    sub esp, len_tc                         ; dd = 4 bytes                       
     
     ; Parameter fungsi akan diambil dengan urutan kanan - kiri
     ; Prototype strncmp: int strncmp(const char* str1, const char* str2, size_t num)
     ; Maka kita harus input parameter dengan urutan tc, str2, dan str1
-    push tc                                 
+    mov ecx, [tc]
+    push ecx                                
     push str2                               
     push str1                               
     call strncmp                            ; panggil fungsi strncmp
@@ -31,8 +34,8 @@ main:
     ; return value dari strncmp disimpan pada register eax
     
     push eax                                ; push retval strncmp ke stack
-    push str1                               ; push str1
-    push str2                               ; push str2
+    push str2                               ; push str1
+    push str1                               ; push str2
     push res                                ; push res / format output
     call printf                             ; panggil fungsi printf
 
