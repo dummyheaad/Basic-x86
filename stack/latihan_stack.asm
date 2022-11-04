a85a03614869fc3f21a4317272c3949b0655fd45
Aysuka Ansari <99653989+dummyheaad@users.noreply.github.com>
2022-11-05 06:48:26 +0700

    Membuat latihan_stack.asm

---
+++ stack/latihan_stack.asm
@@ -0,0 +1,64 @@
+global _start
+
+section .data
+    var1 db 21
+    var2 db 7
+
+    var3 equ 13
+
+section .text
+_start:
+    mov ebp, 3                  ; Inisialisasi nilai ebp
+
+    mov esi, 55                 ; Parameter 1 fungsi tambahkan
+    mov edi, 11                 ; Parameter 2 fungsi tambahkan
+    
+    push esi                    ; Param 1 di-push ke stack
+    push edi                    ; Param 2 di-push ke stack
+
+    call tambahkurang              ; Panggil fungsi tambahkurang
+
+    ; nilai ebx = 55 + 11 - 7 - 13 - 21 = 25
+
+    add ebx, ebp                ; ebx += ebp. Nilai ebp adalah 3 karena sudah di-pop dari stack
+
+    ; nilai ebx = 25 + 3 = 28
+
+    ; gunakan perintah echo $? di terminal linux untuk mencetak nilai exit status dari program
+
+    mov eax, 1
+    int 0x80
+
+tambahkurang:
+    push ebp                    ; Prologue
+    mov ebp, esp
+
+    mov esi, [var1]             ; Var lokal 1
+    push esi                    ; Var lokal 1 di-push
+    mov esi, [var2]             ; Var lokal 2
+    push esi                    ; Var lokal 2 di-push
+
+    ; Push variabel (initialized) harus melalui register terlebih dahulu
+
+    push var3                   ; Var lokal 3
+
+    ; Push immediate value / konstanta bisa dilakukan secara langsung
+
+    mov ebx, [ebp+12]           ; ebx = [esi]
+    add ebx, [ebp+8]            ; ebx += [edi]
+    sub ebx, [ebp-4]            ; ebx -= var1
+    sub ebx, [ebp-8]            ; ebx -= var2
+    sub ebx, [ebp-12]           ; ebx -= var3
+
+    mov esp, ebp                ; Epilogue
+    pop ebp                     ; Kembalikan nilai awal ebp, yaitu 3
+    ret
+
+;           x86_stack
+; ebp+12      [esi]
+; ebp+8       [edi]
+; ebp+4       ret_addr
+; ebp         [ebp]
+; ebp-4       var1
+; ebp-8       var2
+; ebp-12      var3        <-- esp
