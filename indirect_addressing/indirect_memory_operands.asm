6496a7c82275dbb38fd385d47d452da8e8b002fb
Aysuka Ansari <99653989+dummyheaad@users.noreply.github.com>
2022-11-23 18:53:56 +0700

    Update indirect_memory_operands.asm

--- indirect_addressing/indirect_memory_operands.asm
+++ indirect_addressing/indirect_memory_operands.asm
@@ -4,7 +4,7 @@
 global _start
 
 section .data
-    myvar db 250
+    myvar db 200, 30, 10, 100
 
 section .text
 _start:
@@ -12,13 +12,20 @@ _start:
 ;   lea ecx, [myvar]    ; Alternatif untuk mengambil effective address dari myvar
     mov ecx, myvar      ; move address dari myvar ke register ecx
                         ; ecx sekarang mengandung address dari myvar
-                        ; register ecx digunakan agar address tidak terpotong karena ukuran register yang tidak memenuhi
+                        ; register ecx digunakan agar address tidak terpotong karena ukuran register yang tidak memenuhi ukuran address
+
     mov ebx, [ecx]      ; Indirect Addressing.
                         ; move value yang disimpan oleh address yang ditunjuk oleh ecx ke ebx, yaitu nilai dari myvar ke ebx
                         ; ebx sekarang mengandung nilai dari myvar, yaitu 250
                         ; analogi nya mirip seperti dereferencing pada c menggunakan *. Disini register ecx dapat dianggap sebagai sebuah variabel pointer
-                        ; mov ebx, [ecx] -> ebx = *ecx = 250
+                        ; mov ebx, [ecx] -> ebx = *ecx = 200
+
+    add ebx, ecx[1]     ; ebx += [ecx + 1] = 200 + 30 = 230
+    sub ebx, ecx[2]     ; ebx -= [ecx + 2] = 230 - 10 = 220
+
+    sub ebx, [ecx + 3]  ; ebx <<= ecx[3] = 220 - 100 = 120
+
     mov eax, 1          ; syscall exit
     int 0x80            ; panggil kernel untuk menjalankan syscall
-    ; exit status: 250
+    ; exit status: 120
 
